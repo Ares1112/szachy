161fb00d0d264f4ae92bcbf451558bbe389b42da
Arkadiusz Blasiak <arkablas@borg.kis.agh.edu.pl>
2015-05-18 10:55:06 +0200

    wyswietlanie

--- szachy.hs
+++ szachy.hs
@@ -1,9 +1,12 @@
 import Data.Char
+import Data.List.Split
 
 data Kolor = Bialy | Czarny deriving (Eq)
 data Bierka = Krol | Hetman | Goniec | Skoczek | Wieza | Pion
 data PoleGry = Empty | PoleGry Bierka Kolor
-data Szachownica = Szachownica [[PoleGry]]
+data SzachownicaKol = SzachownicaKol PoleGry
+data SzachownicaRza = SzachownicaRza [SzachownicaKol]
+data Szachownica = Szachownica [SzachownicaRza]
 
 poleToString (Empty) = '.'
 
@@ -17,3 +20,32 @@ bierkaToChar Skoczek = 'N'
 bierkaToChar Goniec = 'B'
 bierkaToChar Hetman = 'Q'
 bierkaToChar Wieza = 'R'
+
+charToBierka 'k' = Krol
+charToBierka 'p' = Pion
+charToBierka 'n' = Skoczek
+charToBierka 'b' = Goniec
+charToBierka 'q' = Hetman
+charToBierka 'r' = Wieza
+
+wyswietlKol (SzachownicaKol k) = charToString(poleToString k)
+
+wyswietlRza (SzachownicaRza []) = "\n"
+wyswietlRza (SzachownicaRza (r:rs)) = wyswietlKol r ++ (wyswietlRza (SzachownicaRza rs))
+
+wyswietlSzachownica (Szachownica []) = "\n koniec \n"
+wyswietlSzachownica (Szachownica (s:ss)) =  wyswietlRza s ++ (wyswietlSzachownica (Szachownica ss))
+
+plansza = "rnbqkbnr\npppppppp\n........\n........\n........\n........\nPPPPPPPP\nRNBQKBNR"  
+
+zwrocRzedy (wejscie) = splitOn "\n" wejscie 
+przerobRzad :: String -> [PoleGry]
+przerobRzad "" = []
+przerobRzad (b:bb) 
+	| isUpper b = PoleGry (charToBierka b) Bialy
+		:przerobRzad(bb)
+	| isLower b = PoleGry (charToBierka b) Czarny
+		:przerobRzad (bb)
+
+charToString :: Char -> String
+charToString = (:[])
