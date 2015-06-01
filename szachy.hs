1134181e1780bfe6518c256bd9011a86be0ec141
Arkadiusz Blasiak <arkablas@borg.kis.agh.edu.pl>
2015-06-01 10:51:44 +0200

    wyswietlanie

--- szachy.hs
+++ szachy.hs
@@ -2,7 +2,7 @@ import Data.Char
 import Data.List.Split
 
 data Kolor = Bialy | Czarny deriving (Eq)
-data Bierka = Krol | Hetman | Goniec | Skoczek | Wieza | Pion
+data Bierka = Pusta | Krol | Hetman | Goniec | Skoczek | Wieza | Pion
 data PoleGry = Empty | PoleGry Bierka Kolor
 data SzachownicaKol = SzachownicaKol PoleGry
 data SzachownicaRza = SzachownicaRza [SzachownicaKol]
@@ -20,6 +20,7 @@ bierkaToChar Skoczek = 'N'
 bierkaToChar Goniec = 'B'
 bierkaToChar Hetman = 'Q'
 bierkaToChar Wieza = 'R'
+bierkaToChar Pusta = '.'
 
 charToBierka 'k' = Krol
 charToBierka 'p' = Pion
@@ -27,31 +28,50 @@ charToBierka 'n' = Skoczek
 charToBierka 'b' = Goniec
 charToBierka 'q' = Hetman
 charToBierka 'r' = Wieza
+charToBierka '.' = Pusta
 
 wyswietlKol (SzachownicaKol k) = [poleToString k]
 
 wyswietlRza (SzachownicaRza []) = "\n"
 wyswietlRza (SzachownicaRza (r:rs)) = wyswietlKol r ++ (wyswietlRza (SzachownicaRza rs))
-wyswietlRzedy [] = "\n"
-wyswietlRzedy(r:rs) = wyswietlRza r ++ wyswietlRzedy rs
 
-wyswietlSzachownica (Szachownica []) = "\n koniec \n"
+wyswietlSzachownica (Szachownica []) = "\n"
 wyswietlSzachownica (Szachownica (s:ss)) =  wyswietlRza s ++ (wyswietlSzachownica (Szachownica ss))
 
-plansza = "rnbqkbnr\npppppppp\n........\n........\n........\n........\nPPPPPPPP\nRNBQKBNR"  
+instance Show Szachownica where
+	show a = wyswietlSzachownica a
 
+plansza = "rnbqkbnr\npppppppp\n........\n........\n........\n........\nPPPPPPPP\nRNBQKBNR"  
+-- zrob split, zwroc liste stringow
 zwrocRzedy (wejscie) = splitOn "\n" wejscie 
+
+-- wez String (1 rzad) i zamien na PoleGry
 przerobRzad :: String -> [PoleGry]
 przerobRzad "" = []
 przerobRzad (b:bb) 
-	| isUpper b = PoleGry (charToBierka b) Bialy
+	| isUpper b = PoleGry (charToBierka b2) Bialy
 		:przerobRzad(bb)
-	| isLower b = PoleGry (charToBierka b) Czarny
+	| otherwise = PoleGry (charToBierka b2) Czarny
 		:przerobRzad (bb)
+	where b2 = toLower b
+
+-- zmapuj rzazd na kol
 przerobNaRza lst = SzachownicaRza $map SzachownicaKol lst  
+
+-- wez liste rzedow i zwroc pola gry
 zwrocListeRzedow :: [String] -> [SzachownicaRza]
 zwrocListeRzedow [] = []
 zwrocListeRzedow (elem:lst) = przerobNaRza (przerobRzad elem)
 	:zwrocListeRzedow(lst) 
-charToString :: Char -> String
-charToString = (:[])
+
+-- zwroc szachownice
+zwrocSzach :: [SzachownicaRza] -> Szachownica
+zwrocSzach lsta = Szachownica(lsta)
+
+-- usuwanie bierki
+usun :: Szachownica -> (Int, Int) -> Szachownica
+usun (Szachownica sz) (x,y) = (SzachownicaRza (sz!!x))!!y
+
+-- przeniesienie bierki
+{-przenies :: Szachownica -> (Int,Int) -> Szachownica
+przenies Szachownica sz (x,y) =  -}
